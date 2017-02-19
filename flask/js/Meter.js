1ddf4f3ce0f0d2736f8c12d4cd642675b743a51d
austindunn7 <dkrux777@gmail.com>
2017-02-19 02:27:29 -0500

    added Meter.js object

---
+++ flask/js/Meter.js
@@ -0,0 +1,29 @@
+function Meter() {
+    this.health = 5;
+
+    this.getHealth = function() {
+        return this.health;
+    };
+
+    this.increment = function() {
+        health++;
+        this.draw();
+    };
+
+    this.decrement = function() {
+        health--;
+        this.draw();
+    };
+
+    this.draw = function() {
+        var elem = document.getElementById("myMeter");
+        var width = 1;
+        var id = setInterval(frame, 10);
+        function frame() {
+            width = 10 * health;
+            elem.style.health = health;
+        }
+    };
+
+    this.draw();
+}
