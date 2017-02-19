24b38d06a5ff778bc29a50993c4f899cdfcd03e9
austindunn7 <dkrux777@gmail.com>
2017-02-19 03:55:11 -0500

    updated TermList.js

--- flask/js/TermList.js
+++ flask/js/TermList.js
@@ -2,13 +2,16 @@ function TermList() {
     this.xhttp = new XMLHttpRequest();
     xhttp.open('GET','/list',false);
     xhttp.send();
-    //firstLine = raw.split('\n')[0];
-    //term = raw.split('(')[0].trim();
 
     this.list = xhttp.responseText;
     list = list.split(', ');
 
-    this.isMember = function() {
-
+    this.isMember = function(var string) {
+        for ( var i = 0; i < list.length; i++) {
+            if (list[i] === string) {
+                return true;
+            }
+        }
+        return false;
     };
 }
